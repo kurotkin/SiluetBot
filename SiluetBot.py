fdd0baaf9b047827af2325cafd42b6aab6910471
Виталий А. Куроткин <kurotkin@tmk.local>
2016-11-08 19:11:04 +0300

    git init

---
+++ SiluetBot.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python3
+# Библиотеки:
+# http://docs.python-requests.org/en/master/user/quickstart/
+# https://pypi.python.org/pypi/python-telegram-bot/#installing
+# pip install PyYAML
+# python -m pip install --upgrade pip
+
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Job
+from telegram import ReplyKeyboardMarkup, ParseMode
+import logging
+import os
+import logging
+import functools
+
+import yaml
+import requests
+import urllib.request
+
+im_sun = 'CAPTURE \xE2\x98\x80'
+# Получаем конфигруационные данные из файла
+config = yaml.load(open('conf.yaml'))
+ur = yaml.load(open('conf_s7-1200.yaml'))
+
+# Базовые настройка логирования
+logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level = logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    update.message.reply_text("Привет, дорогой друг!\n Для получения информации набери /info")
+
+def help(bot, update):
+    update.message.reply_text("Для получения информации набери /info <- или нажми")
+
+def tempOut(bot, update):
+    # Температура
+    t_url = ur['S7_1200']['url'] + ur['S7_1200']['Out']['Temp']
+    t = requests.get(t_url)
+    update.message.reply_text("Температура на улице " + t.text + " градусов")
+    # Влажность
+    d_url = ur['S7_1200']['url'] + ur['S7_1200']['Out']['Dump']
+    d = requests.get(d_url)
+    update.message.reply_text("Влажность " + d.text + " %")
+    # Яркость
+    l_url = ur['S7_1200']['url'] + ur['S7_1200']['Out']['Light']
+    l = requests.get(l_url)
+    update.message.reply_text("Солнышко светит на  " + l.text + " непонятных единиц.")
+    bot.sendPhoto(chat_id = update.message.chat_id, photo = 'https://telegram.org/img/t_logo.png')
+
+def info(bot, update):
+    tempOut(bot, update)
+
+def echo(bot, update):
+    #update.message.reply_text(update.message.text)
+    update.message.reply_text("Для получения информации набери /info <- или нажми")
+
+def auth(bot, update):
+    if config['telegtam']['password'] in update.message.text:
+        if update.message.chat_id not in config['telegtam']['authenticated_users']:
+            config['telegtam']['authenticated_users'].append(update.message.chat_id)
+        custom_keyboard = [
+            ['Включить_обогреватели', '/Выключить_обогреватели'],
+            ['/Включить_прожектор', '/Выключить_прожектор'],
+            ['/Температура']
+        ]
+        reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+        bot.sendMessage(
+            chat_id = update.message.chat_id,
+            text = "Вы авторизованы.",
+            reply_markup = reply_markup
+        )
+    else:
+        bot.sendMessage(chat_id = update.message.chat_id, text = "Неправильный пароль.")
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+### Декораторы ###
+# Аутентификация
+def auth_required(func):
+    @functools.wraps(func)
+    def wrapped(bot, update):
+        if update.message.chat_id not in config['telegtam']['authenticated_users']:
+            bot.sendMessage(
+                chat_id = update.message.chat_id,
+                text = "Вы неавторизованы.\nДля авторизации отправьте /auth password."
+            )
+        else:
+            return func(bot, update)
+    return wrapped
+
+# Логирование
+def log(func):
+    @functools.wraps(func)
+    def wrapped(bot, update):
+        logger.info('Received message: {}'.format(
+            update.message.text if update.message else update.callback_query.data)
+        )
+        func(bot, update)
+        logger.info('Response was sent')
+    return wrapped
+
+
+### Команды ###
+@log
+@auth_required
+def outdoor_light_on(bot, update):
+    logger.info('outdoor_light_on')
+    bot.sendMessage(chat_id = update.message.chat_id, text = 'outdoor_light_on')
+
+@log
+@auth_required
+def outdoor_light_off(bot, update):
+    logger.info('outdoor_light_off')
+    bot.sendMessage(chat_id = update.message.chat_id, text = 'outdoor_light_off')
+
+
+@log
+@auth_required
+def heaters_on(bot, update):
+    logger.info('heaters_on')
+    bot.sendMessage(chat_id = update.message.chat_id, text = 'heaters_on')
+
+
+@log
+@auth_required
+def heaters_off(bot, update):
+    logger.info('outdoor_light_off')
+    bot.sendMessage(chat_id = update.message.chat_id, text = 'outdoor_light_off')
+
+
+
+def main():
+    updater = Updater(config['telegtam']['TOKEN'])
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("info", info))
+    dp.add_handler(CommandHandler('Температура', tempOut))
+
+    dp.add_handler(CommandHandler('auth', auth))
+
+    dp.add_handler(CommandHandler('Включить_обогреватели', heaters_on))
+    dp.add_handler(CommandHandler('Выключить_обогреватели', heaters_off))
+    dp.add_handler(CommandHandler('Включить_прожектор', outdoor_light_on))
+    dp.add_handler(CommandHandler('Выключить_прожектор', outdoor_light_off))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+
+
+    dp.add_error_handler(error)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
